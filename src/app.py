ac71326ce83cd494173ab358063423414e22280f
toratte_fukui <toratte.fukui@gmail.com>
2025-06-09 20:32:39 +0900

    yamlをutf-8に

--- src/app.py
+++ src/app.py
@@ -1,5 +1,4 @@
 import logging
-import os
 import re
 import tempfile
 import time
@@ -78,24 +77,36 @@ def save_error_log():
     print(traceback.format_exc())
 
 
-def find_new_jobs_element(driver: webdriver.Chrome) -> int | None:
+def find_new_jobs_element(driver: webdriver.Chrome, new_job_text: str) -> int | None:
     """新着案件の数を返す"""
-    pattern = re.compile(r"新着.*\s*\d+\s*件")
-    elements = driver.find_elements(By.XPATH, "//*[contains(text(),'新着案件')]")
-    for el in elements:
-        text = el.text.replace("\u3000", " ")  # 全角スペース→半角スペース
-        match = pattern.search(text)
-        if match:
-            # logger.info(text)
-            number = re.search(r"\d+", match.group())
-            return int(number.group())
+    # 「新着案件：X件」というテキストを含む要素を探す
+    body_text = driver.find_element(By.TAG_NAME, "body").text
+    with open(f"{time.strftime('logs/%Y%m%d%H%M%S_body_text.txt')}", "w") as f:
+        f.write(body_text)
+    match = re.search(rf"{new_job_text}.*(\d+)件", body_text)
+    if match:
+        return int(match.group(1))
     return None
+
+
+def click_update_button(driver: webdriver.Chrome, update_button_text: str):
+    """更新ボタンをクリックする"""
+    elems = driver.find_elements(By.TAG_NAME, "p")
+    for elem in elems:
+        print(f"div.text: {elem.text}")
+        print("---")
+        if update_button_text in elem.text:
+            elem.click()
+            logger.info("「新規案件を更新」ボタンをクリックしました。")
+            return
+
+    raise Exception("「新規案件を更新」ボタンの要素が見つかりませんでした")
 
 
 if __name__ == "__main__":
     # 設定をsettings.yamlから読み込む
     try:
-        with open("settings.yaml", "r") as f:
+        with open("settings.yaml", "r", encoding="utf-8") as f:
             settings = yaml.safe_load(f)
         URL = settings["URL"]  # ログインページURL
         ROOM_ID = settings["ROOM_ID"]  # ChatWorkルームID
@@ -104,15 +115,19 @@ if __name__ == "__main__":
         MESSAGE = settings["MESSAGE"]  # 通知メッセージ
         API_KEY = settings["API_KEY"]  # ChatWork APIキー
         INTERVAL_SEC = settings["INTERVAL"]  # 監視間隔（秒）
-    except Exception as e:
-        logger.error(f"設定ファイルの読み込みに失敗しました: {e}")
-        raise e
-
-    # chrome起動
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(options=options)
+        NEW_JOB_TEXT = settings["NEW_JOB_TEXT"]  # 新着案件のテキスト
+        UPDATE_BUTTON_TEXT = settings["UPDATE_BUTTON_TEXT"]  # 更新ボタンのテキスト
+    except Exception:
+        logger.error("設定ファイルの読み込みに失敗しました:")
+        logger.error(traceback.format_exc())
+        input("Enterキーを押して終了...")
+        exit(1)
 
     try:
+        # chrome起動
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(options=options)
+
         time.sleep(5)
         logger.info("Chrome起動")
         driver.get(URL)
@@ -136,7 +151,7 @@ if __name__ == "__main__":
         password_input.send_keys(PASS)
         logger.info("ログインボタンクリック")
         submit_button = driver.find_element(By.XPATH, "//button[@type='submit']")
-        submit_button.click()
+        # submit_button.click()
 
         logger.info("//////////////////////////////////////////////////////////////")
         logger.info("// !!!ユーザー操作待機中!!!")
@@ -152,11 +167,12 @@ if __name__ == "__main__":
             try:
                 # 新着案件数の確認
                 new_jobs = WebDriverWait(driver, 10).until(
-                    lambda d: find_new_jobs_element(d)
+                    lambda d: find_new_jobs_element(d, NEW_JOB_TEXT)
                 )
-                logger.info(f"新着案件数: {new_jobs}件")
+                logger.info(f"新着案件: {new_jobs}件")
 
                 if old_jobs and new_jobs > old_jobs:
+                    logger.info(f"新着案件数が増加: {old_jobs}件 -> {new_jobs}件")
                     # 差分を通知
                     send_chatwork_message(
                         API_KEY, ROOM_ID, f"{MESSAGE} ({str(new_jobs)}件)"
@@ -171,14 +187,7 @@ if __name__ == "__main__":
 
             time.sleep(int(INTERVAL_SEC))
 
-            update_buttons = driver.find_elements(
-                By.XPATH, "//*[contains(text(),'新規案件を更新')]"
-            )
-            if update_buttons:
-                update_buttons[0].click()
-                logger.info("「新規案件を更新」ボタンをクリックしました。")
-            else:
-                raise Exception("「新規案件を更新」ボタンのクリックに失敗しました")
+            click_update_button(driver, UPDATE_BUTTON_TEXT)
 
     except Exception as e:
         # エラーをログに保存
